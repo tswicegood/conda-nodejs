9d51dc3c8d9cf90c01ad3eed651cc5084f538988
Marcos Vanetta <marcosvanetta at gmail dot com>
2016-03-14 17:45:03 -0500

    Having node working in windows

--- bld.bat
+++ bld.bat
@@ -1,8 +1,8 @@
 msiexec /a node-v%PKG_VERSION%-x64.msi /qb TARGETDIR=%TEMP%
+mkdir %SCRIPTS%
 
 copy %TEMP%\nodejs\node.exe %SCRIPTS%
-del %SRC_DIR%\nodejs\npm.cmd
-xcopy /i /e /y %SRC_DIR%\nodejs\node_modules %LIBRARY_LIB%\node_modules
+xcopy /i /e /y %TEMP%\nodejs\node_modules %LIBRARY_LIB%\node_modules
 
 echo @echo off > %SCRIPTS%\npm.cmd
 echo node "%LIBRARY_LIB%\.\node_modules\npm\bin\npm-cli.js" %%* >> %SCRIPTS%\npm.cmd
